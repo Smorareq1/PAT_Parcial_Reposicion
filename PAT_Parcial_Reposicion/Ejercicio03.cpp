bac10a6d5b2559489f244d9772ab75aec6a164d7
Smorareq1 <smorareq1@gmail.com>
2023-11-12 13:02:09 -0600

    Todo completado

--- PAT_Parcial_Reposicion/Ejercicio03.cpp
+++ PAT_Parcial_Reposicion/Ejercicio03.cpp
@@ -2,5 +2,28 @@
 
 Node<int>* Ejercicio03::detectCycle(Node<int>* head)
 {
-	return nullptr;
+    Node<int>* corto = head;
+    Node<int>* large = head;
+
+    while (corto != nullptr && large != nullptr && large->next != nullptr) {
+        corto = corto->next;
+        large = large->next->next;
+
+        if (corto == large) {
+            break; //Hay ciclo
+        }
+    }
+
+    if (large == nullptr || large->next == nullptr) {
+        return nullptr; // no hay ciclo
+    }
+
+    corto = head;
+
+    while (corto != large) {
+        corto = corto->next;
+        large = large->next;
+    }
+
+    return large;
 }
