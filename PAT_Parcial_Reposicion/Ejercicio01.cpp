a2c4dfb8af1daa59255c9bc2962308344daa6a36
Smorareq1 <smorareq1@gmail.com>
2023-11-12 12:32:45 -0600

    Ejercicio 01 completado

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -1,6 +1,68 @@
 #include "Ejercicio01.h"
 
+
+Node<int>* Ejercicio01::reverseLinkedList(Node<int>* head) { //reverse como en clase
+    Node<int>* next = nullptr;
+    Node<int>* prev = nullptr;
+    Node<int>* current = head;
+
+
+    while (current != nullptr) {
+        Node<int>* nxt = current->next;
+        current->next = prev;
+        prev = current;
+        current = nxt;
+    }
+    return prev;
+}
+
+
+
+Node<int>* Ejercicio01::Helper(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2) {
+    Node<int>* dummy = new Node<int>{ 0 };
+    Node<int>* tail = dummy;
+    int carry = 0;
+
+    while (sizeL1 > 0 || sizeL2 > 0 || carry != 0) {
+        int num1 = (sizeL1 > 0 && l1 != nullptr) ? l1->value : 0;
+        int num2 = (sizeL2 > 0 && l2 != nullptr) ? l2->value : 0;
+
+        int sum = num1 + num2 + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        Node<int>* newNode = new Node<int>{ digit };
+        tail->next = newNode;
+        tail = tail->next;
+
+        // Mover a los nodos siguientes solo si hay nodos restantes
+        if (sizeL1 > 0 && l1 != nullptr) {
+            l1 = l1->next;
+            sizeL1--;
+        }
+        if (sizeL2 > 0 && l2 != nullptr) {
+            l2 = l2->next;
+            sizeL2--;
+        }
+    }
+
+    Node<int>* result = dummy->next;
+    return result;
+}
+
+
+
+
+
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2)
 {
-    return nullptr;
+    l1 = reverseLinkedList(l1);
+    l2 = reverseLinkedList(l2);
+
+
+    Node<int>* result = Helper(l1, sizeL1, l2, sizeL2);
+
+
+
+    return reverseLinkedList(result);
 }
--- PAT_Parcial_Reposicion/Ejercicio01.h
+++ PAT_Parcial_Reposicion/Ejercicio01.h
@@ -4,6 +4,12 @@
 
 class Ejercicio01
 {
+
+private: 
+		Node<int>* reverseLinkedList(Node<int>* head);
+		Node<int>* Helper(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2);
 public:
 	Node<int>* addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2);
+
+
 };
